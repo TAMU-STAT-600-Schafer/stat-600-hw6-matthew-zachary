ca041975063e46f0d941d91570077a8848c4b4bd
Matthew Blackley <matt9black@gmail.com>
2024-10-29 12:08:45 -0500

    Initial K-means

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -21,6 +21,31 @@ arma::uvec MyKmeans_c(const arma::mat& X, int K,
     // Initialize any additional parameters if needed
     
     // For loop with kmeans algorithm
+    for(int i = 0; i < numIter, ++i){
+  // This calculates which cluster is closest to the data point
+      Y <- apply(euc_function(X, X_squared, mu_1), MARGIN = 1, FUN = which.min);
+      
+  // This updates the cluster means
+      for(i in 1:K){
+        mu_temp[i, ] <- colMeans(X[Y == i, , drop = FALSE])
+      }
+      
+  // This breaks the loop if the cluster means did not change from the previous iteration
+      if(identical(mu_1,mu_temp)){
+        break
+      }
+      
+  // This checks to see if any of the clusters has disappeared and throws an error if one has
+      if(length(unique(Y)) < K){
+        stop(paste("One of the clusters has disappeared"))
+      }
+      
+  // This resets the cluster means
+      mu_1 <- mu_temp
+    }
+    
+  // This removes the naming conventions of Y
+    Y <- unname(Y)
     
     
     // Returns the vector of cluster assignments
