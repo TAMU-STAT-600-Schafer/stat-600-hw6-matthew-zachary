d8d34c25f644836d916a31ac12e11824d68f5403
Matthew Blackley <matt9black@gmail.com>
2024-11-02 22:13:51 -0500

    Adding my initial Kmeans function

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -8,26 +8,102 @@
 //
 // [[Rcpp::depends(RcppArmadillo)]]
 
+
 // [[Rcpp::export]]
-bool identical(const arma::vec& a, const arma::vec& b){
-  // This checks to see if the vectors are the same length
-  if(a.size() != b.size()){
-    return false;
+arma::mat square_mat(const arma::mat& X, int& K){
+  arma::vec squared;
+  arma::mat X_squared(X.n_rows, X.n_cols);
+  arma::mat returner(X.n_rows, K);
+  
+  for(int i = 0; i < X.n_cols; ++i){
+    X_squared.col(i) = arma::square(X.col(i));
   }
   
-  // This checks to see if the vectors are the same values 
-  for(int i = 0; i < a.size(); i++){
-    if(a[i] != b[i]){
-      return false;
+  squared = sum(X_squared,1);
+  
+  for(int i = 0; i < K; ++i){
+    returner.col(i) = squared;
+  }
+  
+  return returner;
+}
+
+//[[Rcpp::export]]
+arma::mat testing(arma::mat& X, arma::mat& mu){
+  arma::mat temp = X * mu.t();
+  return temp;
+}
+
+
+// [[Rcpp::export]]
+arma::mat euc_function_c(const arma::mat& X, const arma::mat& X_squared, const arma::mat& mu){
+  arma::mat cluster;
+  arma::mat temp(mu.n_rows, 1);
+  arma::mat temp_squared(X.n_rows, mu.n_rows);
+  arma::mat returner(X.n_rows, mu.n_rows);
+  
+  temp = sum(arma::square(mu),1);
+  
+  for(int i = 0; i < mu.n_rows; ++i){
+    for(int j = 0; j < X.n_rows; ++j){
+      temp_squared.col(i).row(j) = temp(i);
     }
   }
   
-  // If they are the exact same, it will return true
-  return true;
+  returner = X_squared + temp_squared - (2 * (X * mu.t()));
+  
+  return returner;
 }
 
+// for(i in 1:K){
+//  mu_temp[i, ] <- colMeans(X[Y == i, , drop = FALSE])
+// }
+
+
 // [[Rcpp::export]]
-arma::mat testing(const arma::mat& X){
-  arma::mat X_squared = sum(arma::pow(X,2), 1);
-  return X_squared;
+arma::uvec MyKmeans_c(const arma::mat& X, int K,
+                      const arma::mat& M, int numIter = 100){
+  // All input is assumed to be correct
+  
+  // Initialize some parameters
+  int n = X.n_rows;
+  // int p = X.n_cols;
+  arma::uvec Y(n); // to store cluster assignments
+  
+  // Initialize any additional parameters if needed
+  arma::uvec indices;
+  arma::mat mu_temp(K, n);
+  arma::mat mu_1(K, n);
+  arma::mat distances(n, K);
+  
+  mu_temp = M;
+  mu_1 = M;
+  
+  // For loop with kmeans algorithm
+  arma::mat X_squared = square_mat(X, K);
+  
+  for(int i = 0; i < numIter; ++i){
+    distances = euc_function_c(X, X_squared, mu_1);
+    
+    for (int i = 0; i < distances.n_rows; ++i) {
+      Y(i) = distances.row(i).index_min(); 
+    }
+    
+    for(int i = 0; i < K; ++i){
+      indices = find(Y == (i));
+      mu_temp.row(i) = mean(X.rows(indices), 0);
+    }
+    
+    if (arma::approx_equal(mu_1, mu_temp, "absdiff", 1e-10)) {
+      std::cout << "Matrices are identical. Breaking out of the loop." << std::endl;
+      break; 
+    }
+    
+    mu_1 = mu_temp;
+    
+  }
+  
+  // Returns the vector of cluster assignments
+  return Y;
 }
+
