c0dd84a3c47c28cdc44f3550c29e5d9d977c8feb
Matthew Blackley <matt9black@gmail.com>
2024-11-04 12:05:20 -0600

    Filling out the Kmeans_wrapper.R

--- R/Kmeans_wrapper.R
+++ R/Kmeans_wrapper.R
@@ -15,6 +15,16 @@ MyKmeans <- function(X, K, M = NULL, numIter = 100){
   n = nrow(X) # number of rows in X
   
   # Check whether M is NULL or not. If NULL, initialize based on K random points from X. If not NULL, check for compatibility with X dimensions.
+  if(is.null(M) == TRUE){
+    draws <- sample(1:nrow(X), K)
+    M <- X[draws, ]
+  }
+  
+  
+  # If M is the right dimensions, this creates the clusters from M
+  else if((is.null(M) == FALSE) && sum(dim(M) == c(K, dim(X)[2])) != 2){
+    stop(paste('dim(M) != K \u00D7 ncol(X)'))
+  }
   
   
   # Call C++ MyKmeans_c function to implement the algorithm
